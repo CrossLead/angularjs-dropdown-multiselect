5084a9270357aa91a70372587851d8a121c51d20
Ben Southgate <bsouthga@gmail.com>
2015-12-03 12:33:53 -0800

    strict equality

--- src/angularjs-dropdown-multiselect.js
+++ src/angularjs-dropdown-multiselect.js
@@ -117,21 +117,22 @@ directiveModule.directive('ngDropdownMultiselect', ['$filter', '$document', '$co
                     });
                 }
 
+                function nKeys(x) { return ; }
+
                 function deepEqual(x, y) {
-                  if ((typeof x == "object" && x != null) && (typeof y == "object" && y != null)) {
+
+                  if ((typeof x === "object" && x !== null) &&
+                      (typeof y === "object" && y !== null)) {
+
                     if (Object.keys(x).length != Object.keys(y).length) return false;
 
                     for (var prop in x) {
-                      if (y.hasOwnProperty(prop)) {
-                        if (!deepEqual(x[prop], y[prop])) return false;
-                      }
-                      else {
-                        return false;
-                      }
+                      if (!(y.hasOwnProperty(prop) && deepEqual(x[prop], y[prop]))) return false;
                     }
 
                     return true;
                   }
+
                   return x === y;
                 }
 
